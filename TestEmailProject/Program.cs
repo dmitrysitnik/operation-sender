825f61259d7c57d22eda28e805bef983908d838b
dmitrysitnik <dmitry.sitnik46@gmail.com>
2019-09-29 20:10:29 +0300

    Initial commit

---
+++ TestEmailProject/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net.Mail;
+
+
+
+
+/// <summary>
+/// test project to send message from code.
+/// </summary>
+namespace TestEmailProject
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            //Send message
+            SendMessage();
+
+        }
+
+
+
+
+
+        protected static void SendMessage()
+        {
+            using (SmtpClient smtp = new SmtpClient("smtp.gmail.com"))
+            {
+                smtp.Credentials = new System.Net.NetworkCredential("test.sitnik@gmail.com", "6n903Zs12U78.");
+                //smtp.UseDefaultCredentials = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.EnableSsl = true;
+
+                using (MailMessage mail = new MailMessage
+                {
+                    From = new MailAddress("test.sitnik@gmail.com")
+                })
+                {
+                    mail.To.Add(new MailAddress("h.a.t.e46@yandex.ru"));
+                    mail.Body = "Test body text";
+                    mail.Subject = "test mail from ds";
+                    smtp.Send(mail);
+                }
+            }
+        }
+    }
+}
